fbb383b24df9fcc5236093bc5601ae1602166743
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-17 17:23:32 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,33 @@
 # Write your code here.
+
+#If there is nobody in line, it should say "The line is currently empty."
+
+def line(customers)
+  
+while customers.length 
+
+
+new_number = 1 
+  customers.each { |i| new_number << i + 1 }
+    Puts "Welcome, #{name}. You are #{number} in line."
+    
+    new_numbers = []
+  numbers.each { |i| new_numbers << i ** 2 }
+  new_numbers
+
+def take_a_number(katz_deli, name)
+  katz_deli = []
+  katz_deli.each do |number|
+    Puts "Welcome, #{name}. You are #{number} in line."
+  end
+    
+  
+ # The method should call out (puts) the person's name along with their position in line
+ 
+ def now_serving
+  # puts the next person in line and then remove them from the front
+end
+  
+  #If there is nobody in line, it should call out (puts) that "There is nobody waiting to be served!".
+  
+  
